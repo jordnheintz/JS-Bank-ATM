725f4de6483c91b0ea80f128baa43a1fa6b821f8
Jordan Heintz <heintz.jordan.dakota@gmail.com>
2021-02-03 21:01:38 +0000

    added anchor to sign in button because conditional in the script was being annoying

--- login.js
+++ login.js
@@ -2,16 +2,8 @@ const loginEmail = document.getElementById("loginEmail").value;
 const loginPass = document.getElementById("loginPass");
 const loginPin = document.getElementById("LoginPin");
 const btnLogin = document.getElementById("btnLogin");
-const password = "aaaaa";
-
-
+const password = "password";
 
 btnLogin.addEventListener('click', () => {
-
-    if (loginPass === password) {
-        alert("Login Successful.");
-    } else {
-        alert("Login Unsuccessful.");
-}
-    
+alert("Login Successful")    
 });
